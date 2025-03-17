c9200212caf60cdaa7b8043dbabe1f509ecd806c
Vinicius Tonetti <vrtonetti@gmail.com>
2025-03-17 11:21:45 -0300

    Cropping Jung

--- brazil_pre_historic_veg_map.R
+++ brazil_pre_historic_veg_map.R
@@ -10,8 +10,25 @@ library(terra)
 
 br <- vect("E:/_PESSOAL/ViniciusT/prehistoric_veg_map_brazil/br_limite/br_merged.shp")
 mb_1985 <- rast("E:/_PESSOAL/ViniciusT/prehistoric_veg_map_brazil/MapBiomascol09/brazil_1985.tif")
+IBGE <- vect("E:/_PESSOAL/ViniciusT/prehistoric_veg_map_brazil/IBGE/Vegetacao_5000mil/Vegetacao_5000.shp")
+jung <- rast("E:/_PESSOAL/ViniciusT/prehistoric_veg_map_brazil/jung/iucn_habitatclassification_composite_1km_ver001/iucn_habitatclassification_composite_1km_ver001/iucn_habitatclassification_composite_1km_lvl2_ver001.tif")
 
 
 # Re projecting IBGE -----------------------------------------------------------
 
-terra::project(br, )
+br_wgs84 <- terra::project(br, "EPSG:4326")
+
+#terra::writeVector(br_wgs84, "E:/_PESSOAL/ViniciusT/prehistoric_veg_map_brazil/br_limite/br_wgs84.shp")
+
+mb_1985_cropBR <- mask(crop(mb_1985, br_wgs84), br_wgs84)
+
+writeRaster(mb_1985_cropBR, "E:/_PESSOAL/ViniciusT/prehistoric_veg_map_brazil/MapBiomascol09/mb_1985_crop_BR.tif")
+
+
+# Cropping Jung map to Brazil
+
+jung_br <- mask(crop(jung, br_wgs84), br_wgs84)
+writeRaster(jung_br, "E:/_PESSOAL/ViniciusT/prehistoric_veg_map_brazil/jung/jung_br.tif")
+
+
+
